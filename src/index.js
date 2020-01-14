3c1988dc39693c6d476b648bdfd71dc721b5ef8c
majoaquino99 <majo.aqh@gmail.com>
2020-01-14 12:06:35 -0600

    Creación de modales login y signup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import { example } from './example.js';
 
 example();
+
+let modalLogin = document.getElementById("id01");
+let modalSignUp = document.getElementById("id02");
+let closeModalLogin = document.getElementById("closeModalLoginIn");
+let closeModalSignUp = document.getElementById("closeModalSignUp");
+let loginButton = document.getElementById("loginButton");
+let signUpButton = document.getElementById("signUpButton");
+
+loginButton.addEventListener("click", () => {
+    modalLogin.style.display = "block";
+});
+
+closeModalLogin.addEventListener("click", () => {
+    modalLogin.style.display = "none";
+});
+ signUpButton.addEventListener("click", () => {
+   modalSignUp.style.display = "block";
+ });
+
+ closeModalSignUp.addEventListener("click", () => {
+    modalSignUp.style.display = "none";
+ });
+
+
