89739804c9faa351cfdc8685d2f11383435107df
Mirey91 <mirey.morales91@gmail.com>
2020-01-16 12:52:36 -0600

    Botones de FB y Google y ligadas con firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { example } from './example.js';
 
 example();
-
+//todo esto iría en el js de login
 let modalLogin = document.getElementById("id01");
 let modalSignUp = document.getElementById("id02");
 let closeModalLogin = document.getElementById("closeModalLoginIn");
@@ -10,6 +10,8 @@ let loginButton = document.getElementById("loginButton");
 let signUpButton = document.getElementById("signUpButton");
 let submitSignUpElement = document.getElementById("submitSignUp");
 let submitloginElement = document.getElementById("submitLogin");
+let facebookSignUpButton = document.getElementById("facebookSignUp");
+let googleSignUpButton = document.getElementById("googleSignUp");
 
 loginButton.addEventListener("click", () => {
   modalLogin.style.display = "block";
@@ -26,30 +28,54 @@ closeModalSignUp.addEventListener("click", () => {
   modalSignUp.style.display = "none";
 });
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    console.log(email);
+    let displayName = user.displayName;
+    let email = user.email;
+    let emailVerified = user.emailVerified;
+    let photoURL = user.photoURL;
+    let isAnonymous = user.isAnonymous;
+    let uid = user.uid;
+    let providerData = user.providerData;
   } else {
-    document.getElementById("msj").innerHTML="Sin cuenta, Por Favor Registrate"
+    document.getElementById("msj").innerHTML = "Sin cuenta, Por Favor Registrate"
   }
+});
+
+facebookSignUpButton.addEventListener("click", () => {
+
+  let provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    let token = result.credential.accessToken;
+    let user = result.user;
+  }).catch(function(error) {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    let email = error.email;
+    let credential = error.credential;
+  });
+});
+
+googleSignUpButton.addEventListener("click", () => {
+let provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  let token = result.credential.accessToken;
+  let user = result.user;
+}).catch(function(error) {
+  let errorCode = error.code;
+  let errorMessage = error.message;
+  let email = error.email;
+  let credential = error.credential; 
+  });
 });
 
 submitSignUpElement.addEventListener("click", () => {
   let emailSignUp = document.getElementById("emailSignUp").value;
   let passwordSignUp = document.getElementById("passwordSignUp").value;
-  console.log("email:" + emailSignUp, "password" + passwordSignUp);
   firebase.auth().createUserWithEmailAndPassword(emailSignUp, passwordSignUp)
     .catch(function (error) {
-      //console.log("entro a la funcion");
-      var errorCode = error.code;
-      var errorMessage = error.message;
+      let errorCode = error.code;
+      let errorMessage = error.message;
       alert(errorMessage);
     });
 });
@@ -58,11 +84,12 @@ submitloginElement.addEventListener("click", () => {
   let emaillogin = document.getElementById("loginUsername").value;
   let passwordlogin = document.getElementById("loginPass").value;
   firebase.auth().signInWithEmailAndPassword(emaillogin, passwordlogin)
-  .catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert(errorMessage);
-  });
-  
+    .catch(function (error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      alert(errorMessage);
+    });
 });
+//hasta acá
+
 
